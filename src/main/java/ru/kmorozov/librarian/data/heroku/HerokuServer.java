66ef3261d6d4daf92dc7b692b3892ebf6535d7e4
kvmorozov <kvmorozov@gmail.com>
2016-10-13 17:27:37 +0300

    zoo data intro stub

--- src/main/java/ru/kmorozov/librarian/data/heroku/HerokuServer.java
+++ src/main/java/ru/kmorozov/librarian/data/heroku/HerokuServer.java
@@ -9,7 +9,9 @@ import org.springframework.context.annotation.Bean;
  * Created by sbt-morozov-kv on 12.10.2016.
  */
 
-@SpringBootApplication
+@SpringBootApplication(scanBasePackages = "ru.kmorozov.librarian.controllers," +
+        "ru.kmorozov.librarian.persistence.config," +
+        "ru.kmorozov.librarian.discovery.config, ru.kmorozov.librarian.data.heroku")
 public class HerokuServer {
 
     public static void main(String args[]) {
--- src/main/java/ru/kmorozov/librarian/data/heroku/ZooController.java
+++ src/main/java/ru/kmorozov/librarian/data/heroku/ZooController.java
@@ -1,14 +1,9 @@
 package ru.kmorozov.librarian.data.heroku;
 
-import org.apache.zookeeper.Watcher;
 import org.apache.zookeeper.ZooKeeper;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
-
-import java.util.concurrent.CountDownLatch;
-
-import static ru.kmorozov.librarian.data.heroku.PostgreZooServer.ZOO_PORT;
 
 /**
  * Created by sbt-morozov-kv on 13.10.2016.
@@ -20,30 +15,18 @@ public class ZooController {
     @Autowired
     private PostgreZooServer server;
 
+    @Autowired
+    private ZooKeeper zooClient;
+
     @RequestMapping("/")
     public String index() {
         return "ZooKeeper server status: " + getServerStatus();
     }
 
     private String getServerStatus() {
-        ZooKeeper zoo;
-
-        try {
-            CountDownLatch connSignal = new CountDownLatch(1);
-            zoo = new ZooKeeper("localhost" + ":" + ZOO_PORT, 5000, event -> {
-                if (event.getState() == Watcher.Event.KeeperState.SyncConnected) {
-                    connSignal.countDown();
-                }
-            });
-
-            connSignal.await();
-        } catch (Exception ex) {
-            return "ERROR";
-        }
-
-        if (zoo == null)
+        if (zooClient == null)
             return "ERROR";
         else
-            return zoo.getState().name();
+            return zooClient.getState().name();
     }
 }
