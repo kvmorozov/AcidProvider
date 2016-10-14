26c277cce8c13703d3130428c016d6533ebd975d
kvmorozov <kvmorozov@gmail.com>
2016-10-14 09:34:30 +0300

    use PORT property

--- src/main/java/ru/kmorozov/librarian/data/heroku/PostgreZooServer.java
+++ src/main/java/ru/kmorozov/librarian/data/heroku/PostgreZooServer.java
@@ -15,7 +15,7 @@ import java.util.Properties;
 @Component
 public class PostgreZooServer {
 
-    public static final int ZOO_PORT = 2128;
+    public static final int ZOO_PORT = 34383;
 
     private final ZooKeeperServerMain server;
     private final ServerConfig serverConfig;
@@ -24,7 +24,7 @@ public class PostgreZooServer {
         Properties properties = new Properties();
         properties.put("dataDir", System.getProperty("java.io.tmpdir"));
         properties.put("dataLogDir", System.getProperty("java.io.tmpdir"));
-        properties.put("clientPort", System.getProperty("PORT"));
+        properties.put("clientPort", ZOO_PORT);
         properties.put("tickTime", 2000);
         properties.put("maxClientCnxns", 20);
 
